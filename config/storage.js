fa843e45894d751b114b40c42b3f9365cc687889
psylogic <nemli.othmane@gmail.com>
2019-05-26 02:46:45 +0100

    Fix Storage Path and Display products images

--- config/storage.js
+++ config/storage.js
@@ -1,10 +1,10 @@
 const multer = require("multer");
 const { path } = require("../utils/helpers");
 exports.storage = {
-  imagePath: "public/images",
+  imagePath: "public/images/upload",
   storageConfig: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "public/images");
+      cb(null, "public/images/upload");
     },
     filename: (req, file, cb) => {
       cb(null, Date.now() + "-" + file.originalname);
